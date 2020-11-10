8722d2d5dbd9ee979833f6d33b1082b3e2d049df
Kesley DEV <kesleygti@gmail.com>
2020-11-10 11:26:29 -0300

    [mob] Add styled-components

--- App.tsx
+++ App.tsx
@@ -2,14 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import styled from 'styled-components/native';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Title>Open up App.tsx to start working on your app!</Title>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const Title = styled.Text`
+  color: red;
+  text-align: center;
+  font-size: 16px;
+`;
 
 const styles = StyleSheet.create({
   container: {

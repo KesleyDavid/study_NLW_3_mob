400d88e6b60a75801916de675a021712195a9d5a
Kesley DEV <kesleygti@gmail.com>
2020-11-13 18:03:18 -0300

    [mob] Add Map

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { Dimensions } from 'react-native';
 
 import styled from 'styled-components/native';
 
 export default function App() {
   return (
     <Container>
-      <StatusBar style="auto" />
+      <MapViewMain initialRegion={{
+        latitude: -16.6356162,
+        longitude: -49.3303268,
+        latitudeDelta: 0.008,
+        longitudeDelta: 0.008,
+      }} />
     </Container>
   );
 }
@@ -16,3 +21,7 @@ const Container = styled.View`
   flex: 1;
 `;
 
+const MapViewMain = styled(MapView)`
+  width: ${Dimensions.get('window').width};
+  height: ${Dimensions.get('window').height};
+`;

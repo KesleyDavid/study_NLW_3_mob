e128bdc52b9e14714ebb8b2342797ec500181d56
Kesley DEV <kesleygti@gmail.com>
2020-12-03 08:38:24 -0300

    [mob] Page => Orphanage Data

--- src/pages/CreateOrphanage/OrphanageData/index.tsx
+++ src/pages/CreateOrphanage/OrphanageData/index.tsx
@@ -1,8 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+
+interface OrphanageDataRouteParams {
+  position: { latitude: number, longitude: number};
+}
 
 import * as S from './styled';
 
 export default function OrphanageData() {
-  return <S.Container />;
+  const route = useRoute();
+  const navigation = useNavigation();
+
+  const [open_on_weekends, setOpenOnWeekends] = useState(false);
+
+  const params = route.params as OrphanageDataRouteParams;
+  const position = params.position;
+
+  function handleCreateOrphanage() {
+    // todo
+  }
+
+  async function handleSelectImages() {
+    const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (status !== 'granted') {
+      alert('Eita! Precisamos de acesso às suas fotos...');
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+  }
+
+  return (
+    <S.Container contentContainerStyle={{ padding: 24 }}>
+      {/* kesdev - contentContainerStyle*/}
+      <S.Title>Dados</S.Title>
+
+      <S.Label>Nome</S.Label>
+      <S.TextInput />
+
+      <S.Label>Sobre</S.Label>
+      <S.TextAreaInput
+        multiline
+      />
+
+      <S.Label>Whatsapp</S.Label>
+      <S.TextInput />
+
+      <S.Label>Fotos</S.Label>
+      <S.ImagesInput onPress={handleSelectImages}>
+        <Feather name="plus" size={24} color="#15B6D6" />
+      </S.ImagesInput>
+
+      <S.Title>Visitação</S.Title>
+
+      <S.Label>Instruções</S.Label>
+      <S.TextAreaInput
+        multiline
+      />
+
+      <S.Label>Horario de visitas</S.Label>
+      <S.TextInput />
+
+      <S.SwitchContainer>
+        <S.Label>Atende final de semana?</S.Label>
+        <Switch 
+          thumbColor="#fff" 
+          trackColor={{ false: '#ccc', true: '#39CC83' }}
+          value={open_on_weekends}
+          onValueChange={setOpenOnWeekends}
+        />
+      </S.SwitchContainer>
+
+      <S.NextButton onPress={handleCreateOrphanage}>
+        <S.NextButtonText>Cadastrar</S.NextButtonText>
+      </S.NextButton>
+    </S.Container>
+  );
 }
 
